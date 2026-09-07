7a2e086f543601dbed3d0d082ba227c4144cc20f
TegarAlfaR <tegaralfa18052004@gmail.com>
2026-09-07 13:06:05 +0700

    number in go

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Tegar")
+	fmt.Println("Tegar Alfa Rizzi")
+	fmt.Println("Tegar Alfa Rizzi"[0])
+	fmt.Println("Tegar Alfa Rizzi"[1])
+	fmt.Println(len("Tegar Alfa Rizzi"))
+}
