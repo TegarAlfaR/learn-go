5ba182cf7a11f7be2e262ea5681b0d1dd131622d
TegarAlfaR <tegaralfa18052004@gmail.com>
2026-09-08 23:17:37 +0700

    recursive func in go

---
+++ recursive-function.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// dengan perulangan
+func factorialLoop(value int) int{
+	result := 1
+
+	for i:=value; i > 0; i--{
+		result *= i
+	}
+
+	return result
+}
+
+// dengan recursive
+func factorialRecursive(value int) int{
+	if value == 1 {
+		return 1
+	} else {
+		return value * factorialRecursive(value - 1)
+	}
+}
+
+
+func main() {
+	fmt.Println(factorialLoop(5))
+
+	fmt.Println(factorialRecursive(5))
+}
