9389198ab65cd8b6321ff0ce6ad38f8b55dc6d45
TegarAlfaR <tegaralfa18052004@gmail.com>
2026-09-09 21:56:18 +0700

    recover in go

--- recover.go
+++ recover.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func endApp(){
 	fmt.Println("end app")
+	message := recover()
+	fmt.Println("error message :", message)
 }
 
 func runApp(error bool){
 	defer endApp()
 	if error {
-		panic("ERROR")
+		panic("ERROR CIHUY")
 	}
 }
 
